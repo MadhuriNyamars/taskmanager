ced838622e92cc40b5a59bac42ad0c51f90e22bc
Madhuri Nyamars <madhurinyamars@Madhuris-Air.lan>
2025-05-05 22:40:07 +0530

    first commt task manager

---
+++ task-manager/src/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WidgetComponent } from './components/widget/widget.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    WidgetComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
